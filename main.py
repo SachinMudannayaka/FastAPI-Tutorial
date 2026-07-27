e1a379bc26d7a80089e38bbb1be2d02367891548
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-27 19:53:19 +0530

    path parameter

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return{"Messege":"Hellow world"}
+
+@app.get("/greet")
+def greet():
+    return{"Messege":"Hellow SACHIN"}
+# Pass path paramter
+@app.get("/greet/{name}")
+def greetname(name: str):
+    return {"Messege":f"Hellow {name}"}
