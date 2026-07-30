93f3e4fa442483a34db635232eb82e0d200ffc66
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-30 08:45:51 +0530

    Update until put method

--- curd.py
+++ curd.py
@@ -59,3 +59,19 @@ def create_book(book: Book):
     new_book = book.model_dump()
     books.append(new_book)
 
+class BookUpdate(BaseModel):
+    title: str
+    author: str
+    publish_date: str 
+
+@app.put("/update/book/{book_id}")
+def update_book(book_id: int,book_update:BookUpdate):
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update.title
+            book["author"] = book_update.author
+            book["publish_date"] = book_update.publish_date
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book Not Found")      
+
+
