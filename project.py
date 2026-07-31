0f21a249da433f646b7a773e023e03f931511923
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-31 08:44:03 +0530

    added a data record into the table

---
+++ project.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI,Depends
+from database import get_db,engine
+from sqlalchemy.orm import session
+import model
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class BookStore(BaseModel):
+    id:int
+    title:str
+    author: str
+    publish_date:str
+
+@app.post("/create/book")
+def create_book(book:BookStore, db:session = Depends(get_db)):
+    new_book = model.Book(id=book.id,title=book.title,author= book.author,publish_date = book.publish_date)
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    return(new_book)
